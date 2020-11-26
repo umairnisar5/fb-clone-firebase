45f8f11305e9607de8cbe69de2005c869d96cb91
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-26 16:24:16 +0500

    update sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+import LocalHospitalIcon from "@material-ui/icons/LocalHospital";
+import EmojiFlagsIcon from "@material-ui/icons/EmojiFlags";
+import PeopleIcon from "@material-ui/icons/People";
+import ChatIcon from "@material-ui/icons/Chat";
+import StorefrontIcon from "@material-ui/icons/Storefront";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import { ExpandMoreOutlined } from '@material-ui/icons';
+
 
 function Sidebar() {
-    return <div className="sidebar"></div>
+    return <div className="sidebar">
+        <SidebarRow src='https://avatars2.githubusercontent.com/u/59221584?s=400&u=fa2672a34b583ad3acf351c1976fc1e3b38795c6&v=4' title="Muhammad Talha" />
+        <SidebarRow Icon={LocalHospitalIcon} title="Covid-19 information center" />
+        <SidebarRow Icon={EmojiFlagsIcon} title="Pages" />
+        <SidebarRow Icon={PeopleIcon} title="Frieds" />
+        <SidebarRow Icon={ChatIcon} title="Messenger" />
+        <SidebarRow Icon={StorefrontIcon} title="Marketplace" />
+        <SidebarRow Icon={VideoLibraryIcon} title="Vedios" />
+        <SidebarRow Icon={ExpandMoreOutlined} title="Marketplace" />
+    </div>
 }
 
-export default Sidebar
+export default Sidebar;
