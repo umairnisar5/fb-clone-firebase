db39b430d0de435213d23460dec62cd1093b073f
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-27 23:43:22 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StateProvider } from "./StateProvider";
+import reducer, {  initialState } from "./reducer";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode initialState={initialState} reducer={reducer}>
+   <StateProvider>
+     <App />
+   </StateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
