2b8d359c3fcde7304e677c016cfea5032cf73fdb
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-11-27 23:28:15 +0500

    update firebase.js
    adding database

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyB_otVdzhuL42CJFCcH0hHlk3-8eRKinYA",
@@ -9,3 +11,12 @@ const firebaseConfig = {
     appId: "1:661859260017:web:73875acab0907d108fadd0",
     measurementId: "G-NZLXEK8YS5"
   };
+
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth =firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
